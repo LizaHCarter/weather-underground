082f6e2564cc466c340a87460c095498617214b7
Liza Carter <lizahcarter@gmail.com>
2014-07-22 13:41:28 -0500

    added avgHigh/Low

--- app/models/weather.js
+++ app/models/weather.js
@@ -5,10 +5,13 @@ function Weather(){}
 
 Weather.high = function(zip, cb){
   var url = 'http://api.wunderground.com/api/6904868a2f38a2ae/forecast/q/'+zip+'.json';
-
+  
   request(url, function(error, response, body){
+    //debugger;
     body = JSON.parse(body);
-    cb(body.forecast.simpleforecast.forecastday[0].high.fahrenheit);
+    var high =body.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+    var temp = high + 'F';
+    cb(temp);
   });
 };
 
@@ -17,7 +20,43 @@ Weather.low = function(zip, cb){
 
   request(url, function(error, response, body){
     body = JSON.parse(body);
-    cb(body.forecast.simpleforecast.forecastday[0].low.fahrenheit);
+    var low =body.forecast.simpleforecast.forecastday[0].low.fahrenheit;
+    var temp = low + 'F';
+    cb(temp);
+  });
+};
+
+
+Weather.avgHigh = function(zip, cb){
+  var url = 'http://api.wunderground.com/api/6904868a2f38a2ae/forecast10day/q/'+zip+'.json';
+  
+  request(url, function(error, response, body){
+    debugger;
+    body = JSON.parse(body);
+    var sum = 0;
+    for(var i=0; i<10; i++){
+      sum += parseInt(body.forecast.simpleforecast.forecastday[i].high.fahrenheit);
+    }
+    var avg = sum / 10;
+    var temp = parseInt(avg) + 'F';
+    cb(temp);
+  });
+};
+
+
+Weather.avgLow = function(zip, cb){
+  var url = 'http://api.wunderground.com/api/6904868a2f38a2ae/forecast10day/q/'+zip+'.json';
+  
+  request(url, function(error, response, body){
+    debugger;
+    body = JSON.parse(body);
+    var sum = 0;
+    for(var i=0; i<10; i++){
+      sum += parseInt(body.forecast.simpleforecast.forecastday[i].low.fahrenheit);
+    }
+    var avg = sum / 10;
+    var temp = parseInt(avg) + 'F';
+    cb(temp);
   });
 };
 module.exports = Weather;
--- test/unit/weather.js
+++ test/unit/weather.js
@@ -9,7 +9,7 @@ var Weather = require('../../app/models/weather');
 describe('Weather', function(){
   describe('.high', function(){
     it('should give us the high temp for today', function(done){
-      Weather.high('37221', function(temp){
+      Weather.high('37217', function(temp){
         expect(temp).to.be.ok;
         expect(temp.length).to.be.at.least(2);
         done();
@@ -19,7 +19,27 @@ describe('Weather', function(){
   });
   describe('.low', function(){
     it('should give us the low temp for today', function(done){
-      Weather.low('37221', function(temp){
+      Weather.low('37217', function(temp){
+        expect(temp).to.be.ok;
+        expect(temp.length).to.be.at.least(2);
+        done();
+        console.log(temp);
+      });
+    });
+  });
+  describe('.avgHigh', function(){
+    it('should give us the avg high temp for 10 day forecast', function(done){
+      Weather.avgHigh('37217', function(temp){
+        expect(temp).to.be.ok;
+        expect(temp.length).to.be.at.least(2);
+        done();
+        console.log(temp);
+      });
+    });
+  });
+  describe('.avgLow', function(){
+    it('should give us the avg low temp for 10 day forecast', function(done){
+      Weather.avgLow('37217', function(temp){
         expect(temp).to.be.ok;
         expect(temp.length).to.be.at.least(2);
         done();
