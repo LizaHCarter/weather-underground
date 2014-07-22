668727919387cca405659fb08ca894df6c8671d8
Liza Carter <lizahcarter@gmail.com>
2014-07-22 14:52:08 -0500

    did the highs/lows/deltas arrayz

--- app/models/weather.js
+++ app/models/weather.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var Calc = require('../../../calculator/app/models/calc');
 var request = require('request');
 function Weather(){}
 
@@ -7,7 +7,6 @@ Weather.high = function(zip, cb){
   var url = 'http://api.wunderground.com/api/6904868a2f38a2ae/forecast/q/'+zip+'.json';
   
   request(url, function(error, response, body){
-    //debugger;
     body = JSON.parse(body);
     var high =body.forecast.simpleforecast.forecastday[0].high.fahrenheit;
     var temp = high + 'F';
@@ -31,15 +30,14 @@ Weather.avgHigh = function(zip, cb){
   var url = 'http://api.wunderground.com/api/6904868a2f38a2ae/forecast10day/q/'+zip+'.json';
   
   request(url, function(error, response, body){
-    debugger;
     body = JSON.parse(body);
     var sum = 0;
     for(var i=0; i<10; i++){
       sum += parseInt(body.forecast.simpleforecast.forecastday[i].high.fahrenheit);
     }
     var avg = sum / 10;
-    var temp = parseInt(avg) + 'F';
-    cb(temp);
+    var temp = parseInt(avg);
+    cb(parseInt(temp));
   });
 };
 
@@ -55,8 +53,94 @@ Weather.avgLow = function(zip, cb){
       sum += parseInt(body.forecast.simpleforecast.forecastday[i].low.fahrenheit);
     }
     var avg = sum / 10;
-    var temp = parseInt(avg) + 'F';
-    cb(temp);
+    var temp = parseInt(avg);
+    cb(parseInt(temp));
   });
 };
+
+Weather.stdevLow = function(zip, cb){
+  var url = 'http://api.wunderground.com/api/6904868a2f38a2ae/forecast10day/q/'+zip+'.json';
+  var diffs = [];
+  request(url, function(error, response, body){
+    body = JSON.parse(body);
+    var sum = 0;
+    for(var i=0; i<10; i++){
+      sum += parseInt(body.forecast.simpleforecast.forecastday[i].low.fahrenheit);
+    }
+    var avg = sum / 10;
+    for(i=0; i<10; i++){
+      body.forecast.simpleforecast.forecastday[i].low.fahrenheit -= avg;
+      diffs.push(Math.pow((body.forecast.simpleforecast.forecastday[i].low.fahrenheit), 2));
+    }
+    var temp = Math.sqrt(Calc.mean(diffs));
+    cb(parseInt(temp));
+  });
+};
+
+Weather.stdevHigh= function(zip, cb){
+  var url = 'http://api.wunderground.com/api/6904868a2f38a2ae/forecast10day/q/'+zip+'.json';
+  var diffs = [];
+  request(url, function(error, response, body){
+    body = JSON.parse(body);
+    var sum = 0;
+    for(var i=0; i<10; i++){
+      sum += parseInt(body.forecast.simpleforecast.forecastday[i].high.fahrenheit);
+    }
+    var avg = sum / 10;
+    for(i=0; i<10; i++){
+      body.forecast.simpleforecast.forecastday[i].high.fahrenheit -= avg;
+      diffs.push(Math.pow((body.forecast.simpleforecast.forecastday[i].high.fahrenheit), 2));
+    }
+    var temp = Math.sqrt(Calc.mean(diffs));
+    cb(parseInt(temp));
+  });
+};
+
+Weather.highs = function(zip, cb){
+  var url = 'http://api.wunderground.com/api/6904868a2f38a2ae/forecast10day/q/'+zip+'.json';
+  var highs = [];
+  request(url, function(error, response, body){
+    body = JSON.parse(body);
+    var forecasts = body.forecast.simpleforecast.forecastday;
+    for(var i=0; i<forecasts.length; i++){
+      highs.push(forecasts[i].high.fahrenheit);
+    }
+    cb(highs);
+  });
+};
+
+Weather.lows = function(zip, cb){
+  var url = 'http://api.wunderground.com/api/6904868a2f38a2ae/forecast10day/q/'+zip+'.json';
+  var lows = [];
+  request(url, function(error, response, body){
+    body = JSON.parse(body);
+    var forecasts = body.forecast.simpleforecast.forecastday;
+    for(var i=0; i<forecasts.length; i++){
+      lows.push(forecasts[i].low.fahrenheit);
+    }
+    cb(lows);
+  });
+};
+
+Weather.deltas= function(zip, cb){
+  var url = 'http://api.wunderground.com/api/6904868a2f38a2ae/forecast10day/q/'+zip+'.json';
+  var lows   = [];
+  var highs  = [];
+  var deltas = [];
+  request(url, function(error, response, body){
+    body = JSON.parse(body);
+    var forecasts = body.forecast.simpleforecast.forecastday;
+    for(var i=0; i<forecasts.length; i++){
+      highs.push(parseInt(forecasts[i].high.fahrenheit));
+    }
+    for(i=0; i<forecasts.length; i++){
+      lows.push(parseInt(forecasts[i].low.fahrenheit));
+    }
+    for(i=0; i<forecasts.length; i++){
+      deltas.push(highs[i]-lows[i]);
+    }
+    cb(deltas);
+  });
+};
+
 module.exports = Weather;
--- test/unit/weather.js
+++ test/unit/weather.js
@@ -31,7 +31,7 @@ describe('Weather', function(){
     it('should give us the avg high temp for 10 day forecast', function(done){
       Weather.avgHigh('37217', function(temp){
         expect(temp).to.be.ok;
-        expect(temp.length).to.be.at.least(2);
+        expect(temp).to.be.within(-50, 150);
         done();
         console.log(temp);
       });
@@ -41,9 +41,59 @@ describe('Weather', function(){
     it('should give us the avg low temp for 10 day forecast', function(done){
       Weather.avgLow('37217', function(temp){
         expect(temp).to.be.ok;
-        expect(temp.length).to.be.at.least(2);
+        expect(temp).to.be.within(-50, 150);
         done();
         console.log(temp);
+      });
+    });
+  });
+  describe('.stdevLow', function(){
+    it('should give us standard deviation of lows', function(done){
+      Weather.stdevLow('37217', function(temp){
+        expect(temp).to.be.ok;
+        expect(temp).to.be.within(1, 10);
+        done();
+        console.log(temp);
+      });
+    });
+  });
+  describe('.stdevHigh', function(){
+    it('should give us standard deviation of lows', function(done){
+      Weather.stdevHigh('37217', function(temp){
+        expect(temp).to.be.ok;
+        expect(temp).to.be.within(1, 10);
+        done();
+        console.log(temp);
+      });
+    });
+  });
+  describe('.highs', function(){
+    it('should list highs for the next 10 days', function(done){
+      Weather.highs('37217', function(highs){
+        expect(highs).to.be.ok;
+        expect(highs).to.have.length(10);
+        done();
+        console.log(highs);
+      });
+    });
+  });
+  describe('.lows', function(){
+    it('should list lows for the next 10 days', function(done){
+      Weather.lows('37217', function(lows){
+        expect(lows).to.be.ok;
+        expect(lows).to.have.length(10);
+        done();
+        console.log(lows);
+      });
+    });
+  });
+  describe('.deltas', function(){
+    it('should list for the next 10 days', function(done){
+      Weather.deltas('37217', function(deltas){
+        expect(deltas).to.be.ok;
+        expect(deltas).to.have.length(10);
+        done();
+        console.log(deltas);
       });
     });
   });
